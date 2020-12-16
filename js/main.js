822a12c66a12b1df207feaa01ff5231ad69e07d5
se-020 <73140370+se-020@users.noreply.github.com>
2020-12-17 06:00:17 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,19 +1,5 @@
 'use strict';
 {
-
-  // const numbers = [];
-  // for (let i = 0; i < 15; i++) {
-  //   numbers.push(i + 1);//i+1にすることで０－１４を１－１５にする
-  // };
-  
-  // const selectedNumbers = [];
-  // for (let i = 0; i < 5; i++) {
-  // // let selectedN = Math.floor(Math.random() * numbers.length);
-  // let selectedN = numbers.splice(Math.floor(Math.random() * numbers.length), 1)[0];//[0]でsplice()で作成した配列のindex[0]番目と指定する。そうしないと配列として指定されることになる。
-  // selectedNumbers.push(selectedN);
-  // };
-
-
   function createColumn(col) {
     const source = [];
     for (let i = 0; i < 15; i++) {
@@ -53,9 +39,7 @@
   const columns = createColumns();
   renderBingo(columns);
 
-  //next number call
-  // document.querySelector('tbody').rows[2].cells[2].classList.add('called');
-  
+  //next number call  
   const numbers = [];
   for (let i = 0; i < 75; i++) {
     numbers[i] = i + 1;
@@ -69,17 +53,15 @@
     if (free.textContent === 'FREE') {
       free.classList.add('called');
     }
-    
+
     let next = numbers.splice(Math.floor(Math.random() * numbers.length), 1)[0]
     nextNum.textContent = next;
-    // console.log(next);
     console.log(nextNum.textContent);
     calledNumbers.push(next);
     calledNum.textContent = calledNumbers;
     for (let i = 0; i < 5; i++) {
       for (let n = 0; n < 5; n++) {
         let cell = document.querySelector('tbody').rows[i].cells[n];
-        // console.log(cell);
         console.log(cell.textContent);
         if (cell.textContent === nextNum.textContent) {
           cell.classList.add('called');
@@ -87,35 +69,4 @@
       }
     }
   });
-
-  //↓自作
-  // const b = [];
-  // const i = [];
-  // const n = [];
-  // const g = [];
-  // const o = [];
-
-  // function getNumbers(retsu, th) {
-    
-  //   const source = [];
-  //   for (let i = 0; i < 15 + 15 * retsu; i++) {
-  //     source[i] = i + 1;
-  //   };
-  
-  //   for (let i = 0; i < 5; i++) {
-  //     th[i] = source.splice(Math.floor(Math.random() * (source.length - 15 * retsu))+ 15 * retsu, 1)[0];
-  //   };
-  // };
-
-  // getNumbers(0, b);
-  // getNumbers(1, i);
-  // getNumbers(2, n);
-  // getNumbers(3, g);
-  // getNumbers(4, o);
-  // console.log(b);
-  // console.log(i);
-  // console.log(n);
-  // console.log(g);
-  // console.log(o);
-  // 自作ここまで↑
 }
